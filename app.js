05582039bf5b430a8982e55ff4542717e3e9de5e
Yves BELLENGER <yvesbellenger@hotmail.com>
2017-10-12 00:35:59 +0200

    blockchain faq

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var botbuilder = require('botbuilder');
+var cognitiveservices = require('botbuilder-cognitiveservices');
 
 // Setup restify
 
@@ -16,22 +17,19 @@ var connector = new botbuilder.ChatConnector({
 
 server.post('/api/messages', connector.listen());
 
-var bot = new botbuilder.UniversalBot(connector, [
-
-    function(session){
-        session.beginDialog('reservation:hotel');
-    },
-    function(session, results){
-        session.dialogData.hotel = results.response;
-        session.send(`Votre récapitulatif: <br/> + 
-        nom : ${session.dialogData.hotel.nom}. <br/>
-        mail : ${session.dialogData.hotel.mail}. <br/>
-        age : ${session.dialogData.hotel.age}. <br/>
-        destination : ${session.dialogData.hotel.destination}. <br/>
-        checking : ${session.dialogData.hotel.checking}. <br/> 
-        nbrNuits : ${session.dialogData.hotel.nbrNuits}`);
-    }
-    
-]);
+var bot = new botbuilder.UniversalBot(connector);
 
 bot.library(require('./dialogs/hotel'));
+
+var recognizer = new cognitiveservices.QnAMakerRecognizer({
+    knowledgeBaseId: 'ba413523-10a8-41c6-96b5-1d7ba350310d',
+    subscriptionKey: '797a1b53a3fb4964bb7cc84d604e0389'
+});
+
+var basicQnAMakerDialog = new cognitiveservices.QnAMakerDialog({
+    recognizers: [recognizer],
+    defaultMessage: "Pas de correspondance !!!",
+    qnaThreshold: 0.3
+});
+
+bot.dialog('/', basicQnAMakerDialog);
