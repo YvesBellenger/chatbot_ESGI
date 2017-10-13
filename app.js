6231b320e1d4096b7b7e11a97f4296bcc5ac324b
Yves BELLENGER <yvesbellenger@hotmail.com>
2017-10-13 14:40:43 +0200

    alarme

--- app.js
+++ app.js
@@ -19,17 +19,5 @@ server.post('/api/messages', connector.listen());
 
 var bot = new botbuilder.UniversalBot(connector);
 
-bot.library(require('./dialogs/hotel'));
-
-var recognizer = new cognitiveservices.QnAMakerRecognizer({
-    knowledgeBaseId: 'ba413523-10a8-41c6-96b5-1d7ba350310d',
-    subscriptionKey: '797a1b53a3fb4964bb7cc84d604e0389'
-});
-
-var basicQnAMakerDialog = new cognitiveservices.QnAMakerDialog({
-    recognizers: [recognizer],
-    defaultMessage: "Pas de correspondance !!!",
-    qnaThreshold: 0.3
-});
-
-bot.dialog('/', basicQnAMakerDialog);
+//bot.library(require('./dialogs/hotel'));
+bot.library(require('./dialogs/alarme'));
---
+++ dialogs/alarme.js
@@ -0,0 +1,41 @@
+// déclarer un builder
+// faire un module.export auquel on donne notre tableau de dialogs
+
+var botbuilder = require('botbuilder');
+const library = new botbuilder.Library('reservation');
+
+library.dialog('createAlarm', [
+    function (session) {
+        session.dialogData.alarm = {};
+        builder.Prompts.text(session, "What would you like to name this alarm?");
+    },
+    function (session, results, next) {
+        if (results.response) {
+            session.dialogData.name = results.response;
+            builder.Prompts.time(session, "What time would you like to set an alarm for?");
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            session.dialogData.time = builder.EntityRecognizer.resolveTime([results.response]);
+        }
+
+        // Return alarm to caller  
+        if (session.dialogData.name && session.dialogData.time) {
+            session.endDialogWithResult({ 
+                response: { name: session.dialogData.name, time: session.dialogData.time } 
+            }); 
+        } else {
+            session.endDialogWithResult({
+                resumed: builder.ResumeReason.notCompleted
+            });
+        }
+    }
+]).endConversationAction('cancelAction', 'Ok, cancel order.', {
+    matches: /^cancel$|^goodbye$/i,
+    confirmPrompt: "Are you sure ?"
+});
+
+module.exports = library;
